a7d20b8c2b63c815c67c150a3a782ce3c49f9585
ebenezerandrade <ebenezer.andrade1@gmail.com>
2015-10-20 02:17:04 -0200

    file deletion listaDeProdutos.jsp and create the connection classes to database

---
+++ src/br/com/arcoplan/util/ConnectionFactory.java
@@ -0,0 +1,21 @@
+package br.com.arcoplan.util;
+
+import java.sql.Connection;
+import java.sql.SQLException;
+
+public class ConnectionFactory {
+
+	public static void main(String[] args) {
+		
+		try
+		{
+			Connection connection = new JDBCConection().getConection();
+			connection.close();
+			System.out.println("desconected database!");
+		}
+		catch(SQLException e)
+		{
+			e.printStackTrace();
+		}
+	}
+}
---
+++ src/br/com/arcoplan/util/JDBCConection.java
@@ -0,0 +1,31 @@
+package br.com.arcoplan.util;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+public class JDBCConection {
+	
+	
+	public Connection getConection(){
+		Connection con = null;
+		
+		try{
+			Class.forName("com.mysql.jdbc.Driver");
+			System.out.println("Conecting database");
+			con = DriverManager.getConnection("jdbc:mysql://localhost:3306/arcoplan","root","ideas");
+	
+		}
+		
+		catch(SQLException e)
+		{
+			System.out.println("Problem database conecting"+e);
+		}
+		catch(ClassNotFoundException cf)
+		{
+			cf.printStackTrace();
+		}
+		
+		return con;
+	}
+}
