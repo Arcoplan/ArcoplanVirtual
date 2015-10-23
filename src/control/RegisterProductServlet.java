c21eb8fc6215fa70e7b5275e777cc21cbd260463
ebenezerandrade <ebenezer.andrade1@gmail.com>
2015-10-23 00:58:32 -0200

    organizing directories to comply with patterns of projects

--- src/control/RegisterProductServlet.java
+++ src/control/RegisterProductServlet.java
@@ -1,4 +1,4 @@
-package servlet;
+package control;
 
 import java.io.IOException;
 import java.io.PrintWriter;
--- src/dao/JDBCConection.java
+++ src/dao/JDBCConection.java
@@ -1,4 +1,4 @@
-package br.com.arcoplan.util;
+package dao;
 
 import java.sql.Connection;
 import java.sql.DriverManager;
